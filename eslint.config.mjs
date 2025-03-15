3ddf1e6ad488b039747f4a7e395be35a041f38a4
jefolidev <jefoliveira279@gmail.com>
2025-03-15 09:22:59 -0300

    update: remoduled project and added eslint and prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import { FlatCompat } from '@eslint/eslintrc'
+import js from '@eslint/js'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+})
+
+export default defineConfig([
+  {
+    extends: compat.extends('@rocketseat/eslint-config/node'),
+
+    rules: {
+      'no-useless-constructor': 'off',
+    },
+  },
+  globalIgnores(['dist/', 'node_modules/', 'data/']),
+])
