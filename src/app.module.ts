7508a7bf8e05a5989ce76cf8904a246d78b5f9ac
jefolidev <jefoliveira279@gmail.com>
2025-03-16 15:07:48 -0300

    updated: created auth decorator

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import { AuthModule } from './auth/auth.module'
+import { AuthModule } from './auth/modules/auth.module'
 import { AuthenicateController } from './controllers/authenticate.controller'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { CreateQuestionController } from './controllers/create-question.controller'
--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,32 +1,18 @@
 import { Controller, Post, UseGuards } from '@nestjs/common'
+import { CurrentUser } from 'src/auth/current-user-decorator'
 
-import { AuthGuard } from '@nestjs/passport'
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
+import type { TokenPayload } from 'src/auth/jwt-strategy'
 import { PrismaService } from 'src/prisma/prisma.service'
 
-// const createQuestionBodySchema = z.object({
-//   title: z.string(),
-//   slug: z.string(),
-//   content: z.string(),
-// })
-
-// type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
-
 @Controller('/questions')
-@UseGuards(AuthGuard('jwt'))
+@UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
   constructor(private prisma: PrismaService) {}
 
   @Post()
-  // @HttpCode(201)
-  // @UsePipes(new ZodValidationPipe(createQuestionBodySchema))
-  async handle() {
-    // @Body() body: CreateQuestionBodySchema
+  async handle(@CurrentUser() user: TokenPayload) {
+    console.log(user)
     return 'oie'
-
-    // const questionData = createQuestionBodySchema.parse(body)
-
-    // return {
-    //   questionData,
-    // }
   }
 }
