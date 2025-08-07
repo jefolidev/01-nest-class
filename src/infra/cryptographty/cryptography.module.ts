55e9a7bc3177e71954ae9ad3e6f05550c9ff2d3f
jefolidev <jefoliveira279@gmail.com>
2025-08-07 05:04:35 -0300

    update: added truth cpryptography

---
+++ src/infra/cryptographty/cryptography.module.ts
@@ -0,0 +1,24 @@
+import { Module } from "@nestjs/common";
+
+import { Encrypter } from "@/domain/forum/application/cryptography/encrypter";
+import { HashCompare } from "@/domain/forum/application/cryptography/hash-compare";
+import { HashGenerator } from "@/domain/forum/application/cryptography/hash-generator";
+
+import { BcryptHasher } from "./bcrypt-hasher";
+import { JwtEncrypter } from "./jwt-encrypter";
+
+@Module({
+  providers: [
+    { provide: Encrypter, useClass: JwtEncrypter },
+    { provide: HashCompare, useClass: BcryptHasher },
+    { provide: HashGenerator, useClass: BcryptHasher },
+  ],
+  exports: [
+    Encrypter,
+    HashCompare,
+    HashGenerator,
+  ],
+})
+export class CryptographyModule {
+
+}
